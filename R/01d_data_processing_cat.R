e3ce774185b609cc60c402651f322439fc907dae
Kyle MacDonald <kylem412@gmail.com>
2019-07-17 10:46:46 -0700

    finished larger cat pilot2

--- R/01d_data_processing_cat.R
+++ R/01d_data_processing_cat.R
@@ -1,11 +1,13 @@
 # CAT JSON to CSV ---------------------------------------------------------
 
 source(here::here("R/soc_info_helpers.R"))
-path <- "data/01_raw_data/cat-v1/production-results/"
+exp_name <- "cat-pilot2"
+path <- paste0("data/01_raw_data/", exp_name ,"/production-results/")
 files <- dir(here(path))
 results <- batch_process_cat(files)
 results_tidy <- tidy_cat_data(results)
 
 write_csv(results_tidy, 
           path = here("/data/02_tidy_data",
-                      "soc-info-cat-pilot1-tidy.csv"))
+            paste("soc-info", exp_name,"tidy.csv", 
+                  sep = "-")))

1d7acd91eba982f86b687d15bc2783eccd91e0ec
Kyle MacDonald <kylem412@gmail.com>
2019-09-05 07:19:14 -0700

    adding data from cat exp

--- R/01d_data_processing_cat.R
+++ R/01d_data_processing_cat.R
@@ -1,12 +1,18 @@
 # CAT JSON to CSV ---------------------------------------------------------
 
-source(here::here("R/soc_info_helpers.R"))
-exp_name <- "cat-pilot2"
+source("R/soc_info_helpers.R")
+exp_name <- "cat-full-exp1"
 path <- paste0("data/01_raw_data/", exp_name ,"/production-results/")
-files <- dir(here(path))
+files <- dir(path)
 results <- batch_process_cat(files)
 results_tidy <- tidy_cat_data(results)
 
+
+# check number of participant in each condition 
+results_tidy %>% 
+  distinct(id, condition) %>% 
+  count(condition)
+  
 write_csv(results_tidy, 
           path = here("/data/02_tidy_data",
             paste("soc-info", exp_name,"tidy.csv", 
